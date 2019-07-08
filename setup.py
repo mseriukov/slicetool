543783334e3e28f2c16bc7d48ee385505bfb5540
Mikhail Seriukov <mseriukov@gmail.com>
2019-07-08 14:14:00 +0700

    Added mesh loading.

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
     name='slicetool',
     version=1.0,
     packages=['slicetool'],
+    install_requires=[
+        'numpy-stl',
+        'matplotlib',
+        'meshcut'
+    ],
     entry_points={'console_scripts': ['slicetool = slicetool.main:main']}
 )
--- slicetool/main.py
+++ slicetool/main.py
@@ -1,6 +1,10 @@
 import argparse
 import os
 import sys
+import numpy
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
 
 def main(args=None):
     parser = argparse.ArgumentParser(prog='slicetool', description='Command line interface to slice STL files.')
@@ -10,14 +14,22 @@ def main(args=None):
 
     input_arg = args.input
     input_path = os.path.expanduser(input_arg)
-    if not os.path.isdir(input_path):
+    print("Input: " + input_path)
+    if not os.path.exists(input_path):
         print('The path specified does not exist')
         sys.exit()
 
     output_arg = args.output
     output_path = os.getcwd()
+    print("Output: " + output_path)
     if output_arg: 
         output_path = os.path.expanduser(output_arg)
-        
-    print("Input: " + input_path)
-    print("Output: " + output_path)
+
+    figure = pyplot.figure()
+    axes = mplot3d.Axes3D(figure)
+
+    your_mesh = mesh.Mesh.from_file(input_path)
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+    scale = your_mesh.points.flatten(-1)
+    axes.auto_scale_xyz(scale, scale, scale)
+    pyplot.show()
